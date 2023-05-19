311bb4af41bc69e58771802ef35f63f9344c326d
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2023-05-19 19:03:55 +0900

    실새 소스코드

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -10,70 +10,75 @@ public class BoardManager : MonoBehaviour
     public static int col = 5; //보드의 열
 
     //보드, 0이면 캔디가 없는 상태, 1이면 캔디가 있는 상태. -1은 보드가 없는 상태
-    public int[,] board =  { { -1, 0, 0, 0, 0 },  { 0, 0, 0, 0, 0 },   { 0, 0, 0, 0, 0 },   { 0, 0, 0, 0, 0 },   { 0, 0, 0, 0, -1 } };
+    public int[,] board =
+        { { -1, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, -1 } };
 
     //캔디들의 오브젝트들을 담은 이중배열
-    public GameObject[,] candyObjectBoard = new GameObject[row,col];
+    public GameObject[,] candyObjectBoard = new GameObject[row, col];
 
     [SerializeField]
     public GameObject candy; //캔디 프리팹
-    
-    public static Vector3 top = new Vector3(0f, 2.5f,0f); //캔디가 생성되는 칸
-    private float spawnCandyTime = 1f; //캔디리스폰시간
-    private float spawnTimer, downTimer = 0f; //캔디 리스폰 타임 계산에 쓰이는 변수
+
+
+    public static Vector3 top = new Vector3(0f, 2.5f, 0f); //캔디가 생성되는 칸
+    private float spawnCandyTime = 0.3f; //캔디리스폰시간
+    private float destroyCandyTime = 0.4f; //캔디리스폰시간
+    private float spawnTimer = 1f, destroyTimer;
 
     private void Update()
-    { 
-        //CandiesDown();
+    {
+        if (CheckEmptyCandy()) makeCandy();
+
     }
     private void Start()
     {
-        if (!CheckBoardFull()) InvokeRepeating("makeCandy", 0f, 0.7f);
-
-       // InvokeRepeating("CandiesDown", 0f, 1f);
+        InvokeRepeating("CheckThreeCandy", 0f, 1f);
     }
 
-    //보드가 다 차있는지 확인한다.
+    //보드가 다 차있는지 확인
     public bool CheckBoardFull()
     {
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                //보드가 있는데 object가 들어가 있지 않다면 false 반환한다.
+                //보드가 있는데 object가 들어가 있지 않다면 false 반환
                 if (board[i, j] != -1 && candyObjectBoard[i, j] == null) return false;
             }
         }
         return true;
     }
 
-    public void CandiesDown()
+
+    //빈 캔디 검사하는 함수
+    //모든 캔디를 검사해서 캔디가 없다면 true를 반환, 캔디가 다 꽉 차있다면 false를 반환
+    //보드가 비어있는지 확인
+    public bool CheckEmptyCandy()
     {
-        //Debug.Log("확인중");
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    if (candyObjectBoard[i, j] != null )
-                    {
-                        if (candyObjectBoard[i, j].GetComponent<Candy>().CheckCandyDown()) candyObjectBoard[i, j].GetComponent<Candy>().CandyMove();   
-                    }
-                    
-                }
-            }
-            
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++)
+                if (board[i, j] == 0) return true;
+        return false;
     }
 
-    //입력받은 좌표를 1로 변경 (입력받은 좌표에 사탕이 있다)
+
+    //입력받은 좌표를 0로 변경 (입력받은 좌표에 사탕이 없다)
     public void BoardChecking(int i, int j, GameObject candy)
     {
+        candy.GetComponent<Candy>().isMoving = false;
         board[i, j] = 1;
         candyObjectBoard[i, j] = candy;
+
     }
 
     //입력받은 좌표를 0로 변경 (입력받은 좌표에 사탕이 없다)
     public void BoardUnChecking(int i, int j)
     {
+        candy.GetComponent<Candy>().isMoving = true;
         board[i, j] = 0;
         candyObjectBoard[i, j] = null;
     }
@@ -82,7 +87,9 @@ public class BoardManager : MonoBehaviour
     //캔디들을 검사해 같은 모양이 세줄 이상 있으면 없어지게 만드는 함수
     public void CheckThreeCandy()
     {
-      
+        if (CheckBoardFull())
+        {
+            Debug.Log("확인중..");
             //가로로 연속된 캔디 검사
             for (int i = 0; i < row; i++)
             {
@@ -91,49 +98,54 @@ public class BoardManager : MonoBehaviour
                 {
                     if (candyObjectBoard[i, j] != null && candyObjectBoard[i, j + 1] != null)
                     {
-                        int currentCandyType = candyObjectBoard[i, j].GetComponent<Candy>().candyType; //현재 검사하는 캔디의 타입
-                        int nextCandyType = candyObjectBoard[i, j + 1].GetComponent<Candy>().candyType; //현재 검사하는 캔디 다음의 타입
+                        int currentCandyType = candyObjectBoard[i, j].GetComponent<Candy>().candyType;
+                        int nextCandyType = candyObjectBoard[i, j + 1].GetComponent<Candy>().candyType;
 
                         //만약 현재 캔디랑 다음캔디의 타입이 같다면 matchCount를 증가시킨다.
-                        if (currentCandyType == nextCandyType)
+                        if (currentCandyType != -1 && currentCandyType == nextCandyType)
                         {
-                            matchCount += 1;
-                            //현재 캔디가 검사의 마지막이고 matchCount가 3개 이상이라면,  
+                            matchCount++;
+                            //마지막이라면
                             if (j == col - 2 && matchCount >= 3)
                             {
-                                //matchCount 만큼 반복하면서 캔디를 없앤다. 
                                 for (int c = 0; c < matchCount; c++)
                                 {
                                     DestroyCandy(candyObjectBoard[i, j + 1 - c]);
-
                                 }
-                                //matchCount를 1로 초기화한다. 
                                 matchCount = 1;
                             }
                         }
                         else
                         {
-                            if (matchCount >= 3)
+                            //현재 캔디랑 다음캔디의 타입이 같지 않다면 matchCount의 개수를 확인하고
+                            //3개 이상이라면 DestroyCandy()함수 호출, matchCount를 1로 초기화
+
+                            if (currentCandyType != -1 && matchCount >= 3)
                             {
-                                for (int c = 0; c < matchCount; c++) DestroyCandy(candyObjectBoard[i, j - c]);
+                                for (int c = 0; c < matchCount; c++)
+                                {
+                                    DestroyCandy(candyObjectBoard[i, j - c]);
+                                }
                                 matchCount = 1;
                             }
                         }
                     }
                 }
             }
-        
-    }
-            
 
-    public void DestroyCandy( GameObject candy)
+        }
+
+    }
+
+    public void DestroyCandy(GameObject candy)
     {
-        if (candy != null )
+        if (candy != null)
         {
+            Debug.Log("펑!");
             BoardUnChecking(candy.GetComponent<Candy>().currentBoardX, candy.GetComponent<Candy>().currentBoardY);
             Destroy(candy);
         }
-        
+
 
     }
 
@@ -141,7 +153,15 @@ public class BoardManager : MonoBehaviour
     //캔디생성함수
     public void makeCandy()
     {
-        Instantiate(candy, top, Quaternion.identity);
+
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnCandyTime)
+        {
+            spawnTimer = 0f;
+            if (board[0, col - 1] != 1)
+                Instantiate(candy, top, Quaternion.identity);
+        }
+
+
     }
-    
 }
--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Candy : MonoBehaviour
 {
     //캔디 종류
-    //blue_candy  0, green_candy  1, orange_candy  2, purple_candy  3, red_candy  4, yellow_cadny  5
+    //none -1 blue_candy  0, green_candy  1, orange_candy  2, purple_candy  3, red_candy  4, yellow_cadny  5
     public int candyType;
+
+    //public bool isCandyMoved = false;
 
     private Vector2 currentPos;     // 현재 위치
     private Vector2 targetPos;       // 목표 위치
@@ -18,15 +20,14 @@ public class Candy : MonoBehaviour
     private float spawnTimer = 0f;    //캔디가 자연스럽게 움직임 계산할 때 사용하는 변수
     private float moveCellTime = 0.2f; //캔디가 한칸을 이동하는데 걸리는 시간
 
-    float downTime = 0.2f;
-    float downTimer = 0f; 
 
     //정육면체의 가로세로 길이 
-    public static float candyHeight = 1.7f;  //한칸의 세로 크기
-    public static float candyWidth = 1.5f;  //한칸의 가로 크기
+    public static float cellWidth = 1.7f;  //한칸의 세로 크기
+    public static float cellHeight = 1.5f;  //한칸의 가로 크기
 
     BoardManager boardManager;
-    [SerializeField]
+
+    //움직이는중
     public bool isMoving = true;
 
     [SerializeField]
@@ -38,129 +39,169 @@ public class Candy : MonoBehaviour
 
         currentBoardX = 0;
         currentBoardY = BoardManager.col - 1;
-        boardManager.BoardChecking(currentBoardX, currentBoardY, gameObject);
 
         //랜덤하게 캔디모양 결정
         int randomIndex = Random.Range(0, sprites.Length);
         GetComponent<SpriteRenderer>().sprite = sprites[randomIndex];
         candyType = randomIndex;
+
+
     }
     private void Update()
     {
-        if (CheckCandyDown()) CandyMove();
+       // if (isMoving)
+      //  {
+            CandyCheckAndDown();
+       // }
+
     }
+
+    //보드언체킹할때까지 내려간다. 내려가고 나면 호출할때까지 멈춘다.
+
+    //밑에 공간이 있는지 확인, 공간이 있다면 이동
+    public void CandyCheckAndDown()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= moveCellTime)
+        {
+            spawnTimer = 0f;
+            if (CheckCandyDown()) CandyMove();
+        }
+    }
+
+    //아래로 내려갈 공간이 있으면 true를 반환, 아니라면 false를 반환
 
     public bool CheckCandyDown()
     {
-   
-               //가장 밑줄에 있을때
-       if (currentBoardX == 4 || currentBoardY == 0)
+        //가장 밑줄에 있을때
+        if (currentBoardX == BoardManager.row - 1 || currentBoardY == 0)
         {
-            Debug.Log("가장 밑줄에 있엉");
-            isMoving = false;
+            boardManager.BoardChecking(currentBoardX, currentBoardY, gameObject);
             return false;
-
         }
         //밑에 공간이 비어있는지 확인, 
-        else if (boardManager.board[currentBoardX + 1, currentBoardY - 1] == 0)
+        if (boardManager.board[currentBoardX + 1, currentBoardY - 1] == 0)
         {
-            //Debug.Log("밑에 공간있어요");
-            isMoving = true;
-          //  boardManager.BoardUnChecking(currentBoardX, currentBoardY);
+            boardManager.BoardUnChecking(currentBoardX, currentBoardY);
             return true;
-
         }
         //   왼쪽이나 오른쪽 아래에 공간이 있을떄
         else if (boardManager.board[currentBoardX + 1, currentBoardY] == 0 || boardManager.board[currentBoardX, currentBoardY - 1] == 0)
         {
-            //Debug.Log(" 왼쪽이나 오른쪽 아래에 공간이 있을떄");
-            isMoving = true;
-          
+            boardManager.BoardUnChecking(currentBoardX, currentBoardY);
             return true;
+
         }
         //움직일 공간이 없을 때
         else
         {
-            Debug.Log("공간 없음");
-            isMoving = false;
-           // boardManager.BoardChecking(currentBoardX, currentBoardY, gameObject);
+            boardManager.BoardChecking(currentBoardX, currentBoardY, gameObject);
             return false;
-
         }
-        
-     
     }
 
+    public bool checkUpperAround(int i, int j)
+    {
+        if (i >= 1 && j < 4)
+        {
+            //입력받은  좌표의 위 캔디가 존재한다면 true 반환, 아니면 false 반환
+            if (boardManager.board[i - 1, j + 1] == 1)
+                return true;
+            else
+                return false;
+        }
+        else return false;
 
+
+    }
+    public bool checkLeftAround(int i, int j)
+    {
+        if (i >= 1 && j >= 1)
+        {
+            if (boardManager.board[i - 1, j - 1] == 1)
+                return true;
+            else return false;
+        }
+        else return false;
+
+    }
+    public bool checkRightAround(int i, int j)
+    {
+        if (i < 4 && j < 4)
+        {
+            if (boardManager.board[i + 1, j + 1] == 1)
+                return true;
+            else return false;
+        }
+        else return false;
+
+    }
 
     //캔디를 아래로 움직인다. 
     //다른 캔디를 만나면, 왼쪽이나 오른쪽으로 내려간다.
-    public void CandyMove() {
+    public void CandyMove()
+    {
 
-         downTimer+=Time.deltaTime; 
-
-        if (downTimer > 0.5f)
+        //밑에 공간이 빈다면 밑으로 내려간다.
+        if (boardManager.board[currentBoardX + 1, currentBoardY - 1] == 0)
         {
-            downTimer = 0f;
-            boardManager.BoardUnChecking(currentBoardX, currentBoardY);
-            if (boardManager.board[currentBoardX + 1, currentBoardY - 1] == 0)
-            {
-                Debug.Log("밑으로 내려가요");
-                CandyMoveDown();
-            }
+            CandyMoveDown();
+        }
+        //왼쪽과 오른쪽 둘다 공간이 있다면 둘중 랜덤하게 간다. 
 
-            //왼쪽과 오른쪽 둘다 공간이 있다면 둘중 랜덤하게 간다. 
-            else if (boardManager.board[currentBoardX + 1, currentBoardY] == 0 && boardManager.board[currentBoardX, currentBoardY - 1] == 0)
+        else if (boardManager.board[currentBoardX + 1, currentBoardY] == 0 && boardManager.board[currentBoardX, currentBoardY - 1] == 0)
+        {
+            int isLeft = Random.Range(0, 2);
+            if (isLeft == 0)
             {
-                Debug.Log("랜덤하게 밑으로 내려가요");
-                int isLeft = Random.Range(0, 2);
-                if (isLeft == 0) CandyMoveLeftDown();
-                else CandyMoveRightDown();
-
+                if (!checkUpperAround(currentBoardX, currentBoardY - 1)) CandyMoveLeftDown();
             }
-            //왼쪽아래에 공간이 남았다면 왼쪽아래로 간다.
-            else if (boardManager.board[currentBoardX, currentBoardY - 1] == 0)
+            else
             {
-                Debug.Log("왼쪽 밑으로 내려가요");
-                CandyMoveLeftDown();
-            }
-            //오른쪽아래에 공간이 남았다면 오른쪽아래로 간다.
-            else if (boardManager.board[currentBoardX + 1, currentBoardY] == 0)
-            {
-                Debug.Log("오른쪽 밑으로 내려가요");
-                CandyMoveRightDown();
+                if (!checkUpperAround(currentBoardX + 1, currentBoardY)) CandyMoveRightDown();
             }
         }
-    } 
+        //왼쪽아래에 공간이 남았다면 왼쪽아래로 간다.
+        else if (boardManager.board[currentBoardX, currentBoardY - 1] == 0)
+        {
+
+            if (!checkUpperAround(currentBoardX, currentBoardY - 1)) CandyMoveLeftDown();
+
+        }
+        //오른쪽아래에 공간이 남았다면 오른쪽아래로 간다.
+        else if (boardManager.board[currentBoardX + 1, currentBoardY] == 0)
+        {
+
+            if (!checkUpperAround(currentBoardX + 1, currentBoardY)) CandyMoveRightDown();
+
+        }
+    }
 
     public void CandyMoveDown()
     {
         currentPos = transform.position; //현재 좌표 저장
-        targetPos = currentPos + new Vector2(0, candyHeight * -1);
+        targetPos = currentPos + new Vector2(0, cellWidth * -1);
         currentBoardX += 1;
         currentBoardY -= 1;
-        boardManager.BoardChecking(currentBoardX, currentBoardY, gameObject);
         StartCoroutine(CandyCellMoveCoroutine(targetPos));
 
-        
+
     }
     public void CandyMoveLeftDown()
     {
         currentPos = transform.position; //현재 좌표 저장
-        targetPos = currentPos + new Vector2(-1 * candyWidth, -0.5f * candyHeight);
+        targetPos = currentPos + new Vector2(-1 * cellHeight, -0.5f * cellWidth);
         currentBoardY -= 1;
-        boardManager.BoardChecking(currentBoardX, currentBoardY, gameObject);
         StartCoroutine(CandyCellMoveCoroutine(targetPos));
-        
+
     }
     public void CandyMoveRightDown()
     {
         currentPos = transform.position; //현재 좌표 저장
-        targetPos = currentPos + new Vector2(candyWidth, -0.5f * candyHeight);
+        targetPos = currentPos + new Vector2(cellHeight, -0.5f * cellWidth);
         currentBoardX += 1;
-        boardManager.BoardChecking(currentBoardX, currentBoardY, gameObject);
         StartCoroutine(CandyCellMoveCoroutine(targetPos));
-        
+
     }
 
     // 목표위치까지 자연스럽게 이동하는 코루틴 함수
@@ -176,7 +217,7 @@ public class Candy : MonoBehaviour
             yield return null;
         }
 
-        transform.position = targetPos  ;
+        transform.position = targetPos;
     }
 
 }
